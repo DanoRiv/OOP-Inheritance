b12e50dbb1beefe48a7fca78bf2df1de74861ef1
DanoRiv <danio260496@gmail.com>
2025-03-09 20:51:24 -0500

    Add rectangle class

---
+++ Inheritance/Geometry/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿namespace Geometry;
+
+public class Rectangle : Square
+{
+  
+    private double _b;
+
+    public Rectangle(string name, double a, double b) : base(name, a)
+    {
+        B = b;
+    }
+
+    public double B { get => _b; set => _b = ValidateB(value); }
+
+    public override double GetArea()
+    {
+        return A * B;
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * (A + B);
+    }
+
+    private double ValidateB(double b)
+    {
+        if (b <= 0)
+        {
+            throw new ArgumentException("The side must be greater than zero.");
+        }
+        return b;
+    }
+}
