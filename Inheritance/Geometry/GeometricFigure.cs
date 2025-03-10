a9af2b1d60ac2f564068fafdb0ecdeb05dd5165f
DanoRiv <danio260496@gmail.com>
2025-03-09 20:50:23 -0500

    Initial config

---
+++ Inheritance/Geometry/GeometricFigure.cs
@@ -0,0 +1,15 @@
+﻿namespace Geometry;
+
+public abstract class GeometricFigure
+{
+    public string Name { get; set; } = string.Empty;
+
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+        return $"{Name,-15} => Area.....: {GetArea(),15:N5} \tPerimeter: {GetPerimeter(),15:N5}";
+    }
+}
+
