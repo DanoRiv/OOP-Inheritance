46d08f32c1abeb6e6c55c61f90d1d9f1c9c6ac40
DanoRiv <danio260496@gmail.com>
2025-03-09 20:50:44 -0500

    Add kite class

---
+++ Inheritance/Geometry/Kite.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Geometry;
+
+public class Kite : Rhombus
+{
+    private double _b;
+    public Kite(string name, double a, double d1, double d2, double b) : base(name, a, d1, d2)
+    {
+        B = b;
+    }
+
+    public double B { get => _b; set => _b = ValidateB(value); }
+
+    public override double GetArea()
+    {
+        return base.GetArea();
+    }
+
+    public override double GetPerimeter()
+    {
+        return 2 * (A + B);
+    }
+
+    private double ValidateB(double b)
+    {
+        if (b <= 0)
+        {
+            throw new ArgumentException("The side must be greater than zero.");
+        }
+        return b;
+    }
+}
