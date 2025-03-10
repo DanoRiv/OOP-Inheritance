848ba8272562fe621553b9bc27385435cd99b1c3
DanoRiv <danio260496@gmail.com>
2025-03-09 20:51:47 -0500

    Add square class

---
+++ Inheritance/Geometry/Square.cs
@@ -0,0 +1,33 @@
+﻿namespace Geometry;
+
+public class Square : GeometricFigure
+{
+    private double _a;
+
+    public Square(string name, double a)
+    {
+        Name = name;
+        A = a;
+    }
+
+    public double A { get => _a; set => _a = ValidateA(value); }
+
+    public override double GetArea()
+    {
+        return Math.Pow(A, 2);
+    }
+
+    public override double GetPerimeter()
+    {
+        return 4*A;
+    }
+
+    private double ValidateA(double a)
+    {
+        if (a <= 0)
+        {
+            throw new ArgumentException("The sides must be greater than zero.");
+        }
+        return a;
+    }
+}
